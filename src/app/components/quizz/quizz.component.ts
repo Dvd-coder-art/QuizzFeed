5b9157d530f3e940a004efe86e78a9a51d51d7a5
David <daviddavi1221@gmail.com>
2025-03-31 20:35:41 -0300

    Calculando o Resultado

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -40,16 +40,37 @@ export class QuizzComponent implements OnInit {
 
   playerChoose(value: string) {
     this.answers.push(value);
-    console.log(this.answers);
+    this.nextStep()
   }
 
-  async nextSteo(){
+  async nextStep(){
     this.questionIndex+=1
 
-    if (this.questionIndex>this.questionIndex){
+    if (this.questionMaxIndex>this.questionIndex){
       this.questionSelected = this.questions[this.questionIndex]
     }else{
+      const finalResult: string = await this.checkResult(this.answers)
       this.finished = true
+      this.answersSelected =  quizz_questions.results[finalResult as keyof
+        typeof quizz_questions.results]
+      console.log(this.answers)
     }
   }
+
+  async checkResult(answers:string[]){
+    const result = answers.reduce((previous,current,index,arr)=>{
+      if(
+        arr.filter(item=> item === previous).length >
+        arr.filter(item => item === current).length 
+      ){
+        return previous
+      }else{
+        return current
+      }
+
+    })
+    return result
+  }
+
+  
 }
