31531e202205840d3edb59ec30fc8b055a46ba2f
David <daviddavi1221@gmail.com>
2025-03-31 20:05:07 -0300

    Criando as regras do jogo

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,30 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; // 🚀 Importação necessária
+
+import quizz_questions from "../../../assets/data/quizz_questions.json"; 
 
 @Component({
   selector: 'app-quizz',
-  imports: [],
+  standalone: true, 
+  imports: [CommonModule], 
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
+export class QuizzComponent implements OnInit {
+  title: string = "";
+  questions: any;
+  questionSelected: any;
 
+  answers: string[] = [];
+  answersSelected: string = "";
 
-export class QuizzComponent implements OnInit{
-  title:string = ""
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
 
-  questions:any 
-  questionSelected:any
+  finished: boolean = false;
 
+  constructor() {}
 
-  answers:string[] = []
-  answersSelected:string = ""
-
-  questionIndex:number = 0
-  questionMaxIndex:number = 0
-
-  finished:boolean = false
-
-  constructor(){}
   ngOnInit(): void {
+    if (quizz_questions) {
+      this.finished = false;
+      this.title = quizz_questions.title;
+
+      this.questions = quizz_questions.questions;
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+
+      this.questionSelected = this.questions[this.questionIndex];
+    }
   }
 
+  playerChoose(value: string) {
+    this.answers.push(value);
+    console.log(this.answers);
+  }
+
+  async nextSteo(){
+    this.questionIndex+=1
+
+    if (this.questionIndex>this.questionIndex){
+      this.questionSelected = this.questions[this.questionIndex]
+    }else{
+      this.finished = true
+    }
+  }
 }
